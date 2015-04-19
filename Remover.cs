f79da16bc4de9e54543a9fca4ede75c9823333ac
Aris Lancrescent <Aris@Ariss-MacBook-Pro.local>
2015-04-19 14:50:47 +0200

    Clears the game world of all trains (except for metro trains) to compensate for Traffic Manager's detachment issue.

--- Remover.cs
+++ Remover.cs
@@ -16,8 +16,11 @@ namespace RemoveStuckTrains
         private Settings _settings;
         private Helper _helper;
 
+        private string _confused = ColossalFramework.Globalization.Locale.Get("VEHICLE_STATUS_CONFUSED");
+
         private bool _initialized;
         private bool _terminated;
+        private bool _cleanslate;
 
         protected bool IsOverwatched()
         {
@@ -45,6 +48,7 @@ namespace RemoveStuckTrains
 
             _initialized = false;
             _terminated = false;
+            _cleanslate = false;
 
             base.OnCreated(threading);
         }
@@ -56,6 +60,7 @@ namespace RemoveStuckTrains
             if (!_helper.GameLoaded)
             {
                 _initialized = false;
+                _cleanslate = false;
                 return;
             }
 
@@ -83,21 +88,42 @@ namespace RemoveStuckTrains
                     SkylinesOverwatch.Settings.Instance.Enable.VehicleMonitor  = true;
 
                     _initialized = true;
+                    _cleanslate = false;
 
                     _helper.Log("Initialized");
+                }
+                else if (!_cleanslate)
+                {
+                    VehicleManager instance = Singleton<VehicleManager>.instance;
+
+                    List<ushort> trains = new List<ushort>();
+                    trains.AddRange(SkylinesOverwatch.Data.Instance.PassengerTrains);
+                    trains.AddRange(SkylinesOverwatch.Data.Instance.CargoTrains);
+
+                    foreach (ushort i in trains)
+                    {
+                        instance.ReleaseVehicle(i);
+
+                        SkylinesOverwatch.Helper.Instance.RequestVehicleRemoval(i);
+                    }
+
+                    _cleanslate = true;
                 }
                 else
                 {
                     VehicleManager instance = Singleton<VehicleManager>.instance;
+                    InstanceID instanceID = new InstanceID();
 
-                    ushort[] trains = SkylinesOverwatch.Data.Instance.Trains;
+                    List<ushort> trains = new List<ushort>();
+                    trains.AddRange(SkylinesOverwatch.Data.Instance.PassengerTrains);
+                    trains.AddRange(SkylinesOverwatch.Data.Instance.CargoTrains);
 
                     foreach (ushort i in trains)
                     {
                         Vehicle train = instance.m_vehicles.m_buffer[(int)i];
 
                         bool isBlocked = train.m_blockCounter == 255;
-                        bool isConfused = (train.m_flags & (Vehicle.Flags.WaitingCargo | Vehicle.Flags.GoingBack)) == Vehicle.Flags.None && train.m_targetBuilding == 0; 
+                        bool isConfused = train.Info.m_vehicleAI.GetLocalizedStatus(i, ref train, out instanceID) == _confused; 
 
                         if (isBlocked || isConfused)
                         {
